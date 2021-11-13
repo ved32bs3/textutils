9dc01d53c5dfb66e1d0eb89bffff140c4b02ee6b
Ved Prajapati <vedprajapati008@gmail.com>
2021-11-13 06:27:12 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Textbox from './Components/Textbox';
+import About from './Components/About';
+import ContactUs from './Components/ContactUs';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  const [Mode, setMode] = useState('light');
+	const [SwitchText, setSwitchText] = useState('Enable greyscale');
+  
+  const toggleMode = () => {
+    if(Mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#495057';
+      document.body.style.color = '#fff';
+      setSwitchText('Disable greyscale');
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = '#eee';
+      document.body.style.color = '#333';
+      setSwitchText('Enable greyscale');
+    }
+  }
+
+  return (
+    <>
+      <Router>
+        <Navbar title="TextUtils" aboutText="About Us" contactText="Contact Us" mode={Mode} switch={SwitchText} toggleMode={toggleMode}/> 
+        <Switch>
+          <Route exact path="/about">
+            <div className="container my-2">
+              <About mode={Mode}/>
+            </div>
+          </Route>
+          <Route exact path="/contact-us">
+            <ContactUs />
+          </Route>
+          <Route exact path="/">
+            <Textbox mode={Mode} heading="Enter your text to analyze it below!!"/>
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Textbox.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+
+export default function Textbox(props) {
+  const [CopyBtn, setCopyBtn] = useState('Copy Text')
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+  
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
+  const handleClearClick = () => {
+    let newText = '';
+    setText(newText);
+  }
+
+  const handleCopyClick = () => {
+    let newText = document.getElementById('textArea'); // Selects the text in textarea as a variable
+    newText.select(); // Selects that variable
+    navigator.clipboard.writeText(newText.value); // Copies that variable to clipboard.
+    setCopyBtn('Text Copied!!');
+    setTimeout(() => {
+      setCopyBtn('Copy Text');
+    }, 1500);
+  }
+
+  const handleSpaClick = () => {
+    let newText = text.split(/[ ]+/); // Declares about more tan 1 space between text.
+    setText(newText.join(" ")); // Removes more than one spaces in text.
+  }
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  }
+
+  const [text, setText] = useState('');
+  // setText("Set Text"); - Correct way to change the state
+  return (
+    <>
+      <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+        <div className="mb-3">
+          <h2 className="my-2">{props.heading}</h2>
+          <div className="mb-3">
+            <label htmlFor="textArea" className="form-label">Edit your text to Uppercase, Lowercase and more with the buttons given below the textarea.</label>
+            <textarea className="form-control border-2" id="textArea" rows="8" value={text} placeholder={props.placeholder} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'whitesmoke', color: props.mode==='dark'?'white':'black'}}></textarea>
+          </div>
+          <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
+          <button className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to Lowercase</button>
+          <button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear text</button>
+          <button className="btn btn-primary mx-2 my-2" onClick={handleCopyClick}>{CopyBtn}</button>
+          <button className="btn btn-primary mx-2 my-2" onClick={handleSpaClick}>Remove extra spaces</button> 
+        </div>  
+      </div>
+      <div className="container">
+        <h2>Your typing summary:</h2>
+        <p>{text.split(' ').length - 1} words and {text.length} characters,
+        <span> {0.008 * text.split(' ').length - 0.008} Minutes to read. (Time taken to read is on average and is estimated.)</span></p>
+        <h3>Preview:</h3>
+        <p>{text===''?'Enter some text above to preview it here.':text}</p>
+      </div>
+    </>
+  )
+}
